9509ed95d7d0fc367c9f094f86a52389f0be4c6c
Elizabeth de la Paz Portal <86503141+elizabthpazp@users.noreply.github.com>
2025-03-29 01:46:52 +0000

    set version

--- dist/components/texts/text-masking.d.ts
+++ dist/components/texts/text-masking.d.ts
@@ -1,10 +1,5 @@
 export default TextMasking;
-/**
- * @param {Object} props
- * @param {string | null} [props.title=null]
- * @param {string | null} [props.image=null]
- */
 declare function TextMasking({ title, image }: {
-    title?: string | null | undefined;
-    image?: string | null | undefined;
+    title?: null | undefined;
+    image?: null | undefined;
 }): any;
